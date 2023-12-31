3af8b975274a01222ec3dbb0226971b693062c36
Anirudh Gupta <146579014+AnirudhG07@users.noreply.github.com>
2023-12-31 21:08:47 +0530

    Create default.php

---
+++ default.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styling.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SKU-Updation</title>
+    </head>
+<body>
+    <div class="header">
+        
+        <img class="logo" src="logo.png" alt="logo" class="logo">
+        <h1 class="top_head1">COMPANY NAME PRIVATE LIMITED<br>Inventory Management System</h1> 
+    </div>
+    <br><br>
+    <body>
+    <h2 class="security_msg"> Security page. Pls click the button below to enter Inventory Management System</h2>
+    <div class="securitybtn">
+    <button onclick="openPopup()">Security button</button>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
+// Just simply the security-web.html, but both these files should be present in the Hosting website 
